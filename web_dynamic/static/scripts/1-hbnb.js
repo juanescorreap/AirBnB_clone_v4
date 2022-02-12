29048ee241bddd506b9fc936225e918b3de31a08
John Doe <juanestebancope@outlook.com>
2022-02-12 16:25:45 -0500

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,14 @@
-$(document).ready(function(){
-
-	$("#amenities_checkbox").change
+const $ = window.$;
+$(document).ready(function () {
+  const amenitiesList = [];
+  $('input').css('margin-left', '10px');
+  $('input[type=checkbox]').click(function () {
+    if ($(this).prop('checked')) {
+      amenitiesList.push($(this).attr('data-name'));
+    } else {
+      const index = amenitiesList.indexOf($(this).attr('data-name'));
+      amenitiesList.splice(index, 1);
+    }
+    $('.amenities h4').text(amenitiesList.join(', ')).css({ width: '220px', height: '16px', overflow: 'hidden', 'text-overflow': 'ellipsis', 'white-space': 'nowrap' });
   });
+});
